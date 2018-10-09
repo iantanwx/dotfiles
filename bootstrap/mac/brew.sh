4a471e8f1f8c2f11422a3cf09586db181695e0c3
Ian Tan <iantanwx@gmail.com>
2018-10-09 16:14:49 +0800

    Clean up brew.sh

--- bootstrap/mac/brew.sh
+++ bootstrap/mac/brew.sh
@@ -5,9 +5,6 @@ set -eu
 # ============================================================================
 # taps
 # ============================================================================
-
-brew tap homebrew/emacs
-
 # brew services launchctl manager -- no formulae
 brew tap homebrew/services
 
@@ -16,9 +13,6 @@ brew tap homebrew/services
 # ============================================================================
 
 brew update
-
-# brew cask completion
-brew install homebrew/completions/brew-cask-completion
 
 # ============================================================================
 # compilers, tools, and libs
@@ -33,11 +27,7 @@ brew install pkg-config
 # general
 # ============================================================================
 
-brew install aspell
 brew install neofetch # prefer over archey
-brew install asciinema
-echo "asciinema auth to link computer to account"
-brew install catimg
 
 # ============================================================================
 # filesystem
@@ -54,37 +44,12 @@ brew install tree
 # OpenSSL for some programs, but prefer libressl where possible
 brew install openssl
 
-# Too annoying to re-setup ssh-agent since the ssh-add does not have keychain
-# access -- disabled:
-# ----------------------------------------------------------------------------
-# Install a newer version of OpenSSH
-# that isn't susceptible to http://www.openssh.com/txt/release-7.1p2
-#brew install homebrew/dupes/ssh --with-libressl
-
-# Since we're not using system ssh, the launchd's attempt to ssh-add our key
-# fails (due to wrong flags). We could change the launchagent, but instead
-# prefer to use keychain to interface with ssh-agent and osxkeychain.
-# It is run in shell/os.bash.
-#brew install keychain
-# ----------------------------------------------------------------------------
-
 brew install htop --with-ncurses
 
-brew install nmap
 brew install ssh-copy-id
 brew install multitail
 
-# Use the `gpgtools` cask instead (it also provides cli `gpg` and
-# `gpg-agent`). It provides a nicer gui prefpane and automatic integration
-# without having to configure all this stuff.
-#brew install gnupg2
-#brew install gpg-agent pinentry-mac
-
 brew install wget
-
-# better curl
-brew install curl --with-libressl
-brew link --force curl
 
 # ============================================================================
 # pretty print and processor
@@ -98,21 +63,12 @@ brew install jsonpp
 # programming
 # ============================================================================
 
-brew install homebrew/homebrew-php/composer
-
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 
 brew install cloc
 
 # used by fzf grepper preview
 brew install highlight
-
-# ios dev
-brew install carthage
-
-brew install lua
-brew install mono
-brew install tidy-html5
 
 # ----------------------------------------------------------------------------
 # git
@@ -133,13 +89,6 @@ brew install bash
 brew install homebrew/versions/bash-completion2
 brew install bats shellcheck
 brew install tmux
-
-# ============================================================================
-# OS
-# ============================================================================
-
-brew install redshift # prefer opensource vs f.lux
-brew install mas
 
 # ============================================================================
 # Finish up
