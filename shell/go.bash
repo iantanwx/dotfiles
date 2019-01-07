8a593bda7c7e7718c2bef15d78562c5f0bc98d4e
Ian Tan <iantanwx@gmail.com>
2019-01-07 12:45:54 +0800

    Fix go env vars

--- shell/go.bash
+++ shell/go.bash
@@ -13,11 +13,18 @@ else
 fi
 
 # GOROOT
-export GOROOT="$(brew --prefix go@1.9)/libexec"
+if [[ "$OSTYPE" == "darwin" ]]; then
+  export GOROOT="$(brew --prefix go@1.9)/libexec"
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  export GOROOT="/usr/local/go/libexec"
+fi
 
 # go -- prefer go binaries over composer; GOPATH is in shell/vars.bash
-export PATH="$(brew --prefix go@1.9)/bin:${PATH}"
-export PATH="${GOPATH}/bin:${GOROOT}/bin:${PATH}"
+if [[ "$OSTYPE" == "darwin" ]]; then
+  export PATH="$(brew --prefix go@1.9)/bin:${PATH}"
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  export PATH="${GOPATH}/bin:${GOROOT}/bin:${PATH}"
+fi
 
 # ==============================================================================
 
