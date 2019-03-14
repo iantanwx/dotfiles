34f3d3bf808d6e8fb06768b6f716f1f75090feb5
Ian Tan <iantanwx@gmail.com>
2019-03-14 11:50:11 +0800

    Do not source llvm-related C flags

--- shell/before.bash
+++ shell/before.bash
@@ -29,6 +29,7 @@ export DKO_SOURCE="${DKO_SOURCE} -> shell/before.bash {"
 . "${DOTFILES}/shell/python.bash"
 . "${DOTFILES}/shell/ruby.bash"
 . "${DOTFILES}/shell/ocaml.bash"
-. "${DOTFILES}/shell/llvm.bash"
+# do not source C/C++ custom settings for now
+# . "${DOTFILES}/shell/llvm.bash"
 
 export DKO_SOURCE="${DKO_SOURCE} }"
